9cd3cfdd050a45abd6ceb2e28e17fabbf8ef4ec8
nupeshpatel4342-ux <nupeshpatel4342@gmail.com>
2026-07-18 02:19:25 +0530

    Remove PushDebug

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,12 +1,10 @@
 import StoreHeader from '@/components/StoreHeader'
 import BottomNav from '@/components/BottomNav'
 import NotificationPrompt from '@/components/NotificationPrompt'
-import PushDebug from '@/components/PushDebug'
 
 export default function StoreLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen bg-slate-50/50 pb-16 lg:pb-0">
-      <PushDebug />
       <NotificationPrompt />
       <StoreHeader />
       <main className="flex-1 overflow-y-auto pb-4">{children}</main>
